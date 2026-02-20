6f96832175c4bb3b2ca56453696b95ba84d4cf64
fudzich <vsevolod656@gmail.com>
2026-02-20 15:24:36 +0300

    Added fade during teleportation

--- VR_Game/Assets/Scripts/LookToTeleport.cs
+++ VR_Game/Assets/Scripts/LookToTeleport.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LookToTeleport : MonoBehaviour
 {
@@ -6,6 +8,9 @@ public class LookToTeleport : MonoBehaviour
     public float teleportDelay = 2f; // Time in seconds to look before teleporting
     public Transform playerTransform; // Player's transform, to teleport
     private float gazeTimer = 0f;
+
+    public Image fadeImage; // UI black Image that is used to blacken screen dring teleportation
+    public float fadeDuration = 1f; // Duration of fade in/out
 
     private OutlineOnLook currentPlatform = null;
     
@@ -36,8 +41,14 @@ public class LookToTeleport : MonoBehaviour
 
                 if (gazeTimer >= teleportDelay)
                 {
+                    //Fade screen to black
+                    StartCoroutine(Fade(0f, 1f, fadeDuration));
                     // Teleport the player
                     TeleportPlayer(currentPlatform.transform);
+                    //Wait for a moment
+                    StartCoroutine(WaitBlack(0.3f));
+                    //Unfade screen from black
+                    StartCoroutine(Fade(1f, 0f, fadeDuration));
                     // Reset timer
                     gazeTimer = 0f;
                 }
@@ -61,5 +72,29 @@ public class LookToTeleport : MonoBehaviour
 
             Debug.Log("Player teleported to platform.");
         }
+    }
+
+    // Wait for a time
+    public IEnumerator WaitBlack(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+
+    // Change transparency of the image over a time
+    private IEnumerator Fade(float startAlpha, float endAlpha, float duration)
+    {
+        float elapsed = 0f;
+        Color color = fadeImage.color;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, endAlpha, elapsed / duration);
+            fadeImage.color = new Color(color.r, color.g, color.b, alpha);
+            yield return null;
+        }
+
+        // Ensure final alpha is set
+        fadeImage.color = new Color(color.r, color.g, color.b, endAlpha);
     }
 }
